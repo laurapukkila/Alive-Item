145cfd121fe385eb59ad7acc78ec1062675c0b5c
laurapukkila <33055297+laurapukkila@users.noreply.github.com>
2017-12-04 21:13:11 +0200

    Add files via upload

---
+++ src/main/java/wad/ProductionConfiguration.java
@@ -0,0 +1,20 @@
+
+package wad;
+
+import javax.activation.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
+public class ProductionConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return (DataSource) DataSourceBuilder.create().build();
+    }
+}
