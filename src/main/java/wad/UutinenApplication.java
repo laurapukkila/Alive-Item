d3a5b7ec88a1c14c37510189a2ba2e0a1c80d4a8
laurapuk <30598632+laurapuk@users.noreply.github.com>
2017-12-10 19:19:28 +0200

    sad

--- src/main/java/wad/UutinenApplication.java
+++ src/main/java/wad/UutinenApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication (scanBasePackages={"wad.configuration", "wad", "wad.controller", "wad.domain", "wad.repository"})
-@EnableJpaRepositories("wad.repository.UutinenRepository")
+@SpringBootApplication
 public class UutinenApplication {
 
     public static void main(String[] args) {
