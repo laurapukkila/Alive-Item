33a4c935b8b60986dfa3d07e74cbb93ef8eb607e
laurapuk <30598632+laurapuk@users.noreply.github.com>
2017-12-10 19:42:28 +0200

    as

--- src/main/java/wad/repository/UutinenRepository.java
+++ src/main/java/wad/repository/UutinenRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wad.domain.Uutinen;
 
-@Repository
+@Component
 public interface UutinenRepository extends JpaRepository<Uutinen, Long> {
 }
